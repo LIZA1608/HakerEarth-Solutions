9f17e08eca3ff030d1fb5930552fab137adb5624
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-19 20:14:00 +0530

    Update Number of steps.cpp

--- Basic Programming/Basics of Input output/Number of steps.cpp
+++ Basic Programming/Basics of Input output/Number of steps.cpp
@@ -1,3 +1,4 @@
+/*https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/make-all-equal-90a21ab2/*/
 #include <iostream>
 using namespace std;
 int main()
