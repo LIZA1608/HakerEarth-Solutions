8b476878b55f6d37a6cebb7fd9c1343286d91d06
LIZA1608 <78140921+LIZA1608@users.noreply.github.com>
2021-07-23 20:26:44 +0530

    Create Robotic moves.cpp

---
+++ Basic Programming/Basics of Implementation/Robotic moves.cpp
@@ -0,0 +1,15 @@
+/*https://www.hackerearth.com/practice/basic-programming/implementation/basics-of-implementation/practice-problems/algorithm/robotic-moves-98b936fb/*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+int t;
+cin >> t;
+while (t--)
+{
+long long int n;
+cin >> n;
+cout << n*(n+1) << endl;
+}
+}
